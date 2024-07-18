7c094f01745ea6bbac1a713d0a89422f37459c01
Chriscastanuela <chriscastanuela@ymail.com>
2024-07-17 23:14:58 -0600

    remove survival guide

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -12,52 +12,52 @@ const ContactUs = () => {
             <div className=" pb-10  flex justify-center items-center flex-col">
                 <div className="bg-contact_bg min-h-[400px] w-full flex justify-center items-center bg-cover bg-center">
                 <h1 className="text-center text-4xl text-white">Contact Us</h1>
-            </div>
-            <div className="w-full md:w-11/12  md:flex-col mx-auto  flex justify-center min-h-[400px] my-6 mt-20 max-w-[1700px] rounded-lg">
-                <div className="w-7/12 h-full p-3 md:w-full px-9">
-                    <h2 className="text-2xl py-7">Get In Touch</h2>
-                    <div className="">
-                        <form name='contact' action="/contact" method="post" netlify='true' className="flex flex-col gap-y-4" id='contact'>
-                            <input type="hidden" name="form-name" value="contact" />
-                            <div className="flex gap-x-4">
-                                <input type="text" name='name' placeholder="Name" className="w-full pl-2 py-2 outline-none  rounded-md border border-gray-300 "  required/>
-                            </div>
-                            <div className="">
-                                <input type="text" name='email' placeholder="Email" className="w-full pl-2 py-2 outline-none  rounded-md border border-gray-300 "  required/>
-                            </div>
-                            <div className="">
-                                <input type="text" name='subject' placeholder="Subject" className="w-full pl-2 py-2 outline-none  rounded-md border border-gray-300 "  required/>
-                            </div>
-                            <div className="">
-                                <select title='Referred by...' name='referred-by' placeholder="Referred by..." required className="w-full pl-2 py-2 outline-none  rounded-md border border-gray-300 custom-select" defaultValue='referred-by'> 
-                                    <option disabled value="referred-by" className=''>
-                                        Referred by...
-                                    </option>
-                                    <option value="Google">Google</option>
-                                    <option value="Facebook">Facebook</option>
-                                    <option value="Instagram">Instagram</option>
-                                    <option value="Jason">Jason</option>
-                                    <option value="Shandra">Shandra</option>
-                                    <option value="Chris">Chris</option>
-                                    <option value="Adam">Adam</option>
-                                    <option value="Other">Other</option>
-                                </select>
-                            </div>
-                            <div className="">
-                                <textarea name="message" id="message" cols="30" rows="10" className="w-full pl-2 py-2 outline-none  rounded-md border border-gray-300 " placeholder="Message" required></textarea>
-
-                            </div>
-                            <button className="w-40 h-12  mt-1 rounded-lg bg-[#f79f24] font-semibold text-white">Send Message</button>
-                        </form>
-                    </div>
                 </div>
-                    <div className="bg-[#f79f24] w-[500px] rounded-lg sm:w-full min-h-full flex flex-col gap-y-14 justify-center pl-7 md:w-full md:py-10 md:gap-y-9">
+                <div className="w-full md:w-11/12  md:flex-col mx-auto  flex justify-center min-h-[400px] my-6 mt-20 max-w-[1700px] rounded-lg">
+                    <div className="w-5/12 h-full p-3 md:w-full px-9">
+                        <h2 className="text-2xl py-7">Get In Touch</h2>
+                        <div className="">
+                            <form name='contact' action="/contact" method="post" netlify='true' className="flex flex-col gap-y-4" id='contact'>
+                                <input type="hidden" name="form-name" value="contact" />
+                                <div className="flex gap-x-4">
+                                    <input type="text" name='name' placeholder="Name" className="w-full pl-2 py-2 outline-none  rounded-md border border-gray-300 "  required/>
+                                </div>
+                                <div className="">
+                                    <input type="text" name='email' placeholder="Email" className="w-full pl-2 py-2 outline-none  rounded-md border border-gray-300 "  required/>
+                                </div>
+                                <div className="">
+                                    <input type="text" name='subject' placeholder="Subject" className="w-full pl-2 py-2 outline-none  rounded-md border border-gray-300 "  required/>
+                                </div>
+                                <div className="">
+                                    <select title='Referred by...' name='referred-by' placeholder="Referred by..." required className="w-full pl-2 py-2 outline-none  rounded-md border border-gray-300 custom-select" defaultValue='referred-by'> 
+                                        <option disabled value="referred-by" className=''>
+                                            Referred by...
+                                        </option>
+                                        <option value="Google">Google</option>
+                                        <option value="Facebook">Facebook</option>
+                                        <option value="Instagram">Instagram</option>
+                                        <option value="Jason">Jason</option>
+                                        <option value="Shandra">Shandra</option>
+                                        <option value="Chris">Chris</option>
+                                        <option value="Adam">Adam</option>
+                                        <option value="Other">Other</option>
+                                    </select>
+                                </div>
+                                <div className="">
+                                    <textarea name="message" id="message" cols="30" rows="10" className="w-full pl-2 py-2 outline-none  rounded-md border border-gray-300 " placeholder="Message" required></textarea>
+
+                                </div>
+                                <button className="w-40 h-12  mt-1 rounded-lg bg-[#f79f24] font-semibold text-white">Send Message</button>
+                            </form>
+                        </div>
+                    </div>
+                    <div className="w-5/12 bg-[#f79f24] rounded-lg sm:w-full min-h-full flex flex-col gap-y-14 justify-center pl-7 md:w-full md:py-10 md:gap-y-9">
                         <div className="flex  items-center gap-x-2 w-full flex-wrap ">
 
                             <span className="w-12 h-12 sm:w-10 sm:h-10  rounded-full flex justify-center items-center bg-[rgba(255_,255_,255_,0.2)]">
                                 <IoLocationSharp className="text-xl sm:text-base text-white" />
                             </span>
-                            <span className="font-semibold text-white">Springs: </span>
+                            <span className="font-semibold text-white">Springs </span>
                             <span className="text-white">1670 Paonia St Suite 120, Colorado Springs, CO 80915</span>
 
                         </div>
@@ -66,7 +66,7 @@ const ContactUs = () => {
                             <span className="w-12 h-12 sm:w-10 sm:h-10  rounded-full flex justify-center items-center bg-[rgba(255_,255_,255_,0.2)]">
                                 <IoLocationSharp className="text-xl sm:text-base text-white" />
                             </span>
-                            <span className="font-semibold text-white">San Angelo: </span>
+                            <span className="font-semibold text-white">San Angelo </span>
                             <span className="text-white">6566 US-67 Suite 99, San Angelo, TX 76901</span>
 
                         </div>
@@ -75,7 +75,7 @@ const ContactUs = () => {
                             <span className="w-12 h-12 sm:w-10 sm:h-10  rounded-full flex justify-center items-center bg-[rgba(255_,255_,255_,0.2)]">
                                 <IoLocationSharp className="text-xl sm:text-base text-white" />
                             </span>
-                            <span className="font-semibold text-white">Midland: </span>
+                            <span className="font-semibold text-white">Midland </span>
                             <span className="text-white">5301 Cholla Rd Suites 41 and 42, Midland, TX 79706</span>
 
                         </div>
@@ -107,9 +107,9 @@ const ContactUs = () => {
                         </div>
 
                     </div>
-                        {/* Just a pic <div className="w-3/6 flex justify-center md:w-full">
-                            <img src="/src/assets/contact.webp" className=" rounded-lg w-9/12 lg:w-11/12 md:w-8/12 h-6/6" alt="" loading='lazy'/>
-                        </div> */}
+                    {/* Just a pic <div className="w-3/6 flex justify-center md:w-full">
+                        <img src="/src/assets/contact.webp" className=" rounded-lg w-9/12 lg:w-11/12 md:w-8/12 h-6/6" alt="" loading='lazy'/>
+                    </div> */}
 
                 </div>
                 {/* <div className="mt-10 flex justify-center items-center">
